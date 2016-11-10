48fba72629dc832f3b9c40d2e52b8ebd02d75534
Scott Bellware <sbellware@gmail.com>
2016-11-10 01:34:00 -0600

    Incorrect arity test

---
+++ test/automated/macro/incorrect_arity.rb
@@ -0,0 +1,22 @@
+require_relative '../automated_init'
+
+context "Apply" do
+  context "Macro" do
+    context "Apply Block Without Parameter" do
+      define_handler = proc do
+        Class.new do
+          include EntityProjection
+
+          apply EntityProjection::Controls::Message::SomeMessage do
+          end
+        end
+      end
+
+      test "Is an error" do
+        assert define_handler do
+          raises_error? EntityProjection::ApplyMacro::Error
+        end
+      end
+    end
+  end
+end
